971f90768ada083d77da589fb040a5b4b0328c2c
sxfang32 <417036650@qq.com>
2019-08-07 17:26:51 +0800

    用户注册逻辑

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -8,3 +8,14 @@ class RegisterView(View):
     def get(self, request):
         """展示用户注册芥界面"""
         return render(request, 'register.html')
+
+    def post(self, request):
+        """用户注册逻辑"""
+        # 接收请求体中的表单数据
+
+        # 校验数据
+
+        # 新增用户记录
+
+        # 跳转到首页
+        pass
