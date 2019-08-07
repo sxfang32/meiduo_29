318c07793b8e08a598ceac076b5413dd696827fb
sxfang32 <417036650@qq.com>
2019-08-07 17:06:46 +0800

    添加模型

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class RegisterView(View):
+    """用户注册"""
+
+    def get(self, request):
+        """展示用户注册芥界面"""
+        return render(request, 'register.html')
