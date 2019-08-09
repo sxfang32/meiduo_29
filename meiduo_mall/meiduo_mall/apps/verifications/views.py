17d4dba9fbc9c4e34869b7bd8161f5f59c0df7e4
sxfang32 <417036650@qq.com>
2019-08-09 21:52:52 +0800

    修正代码

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -40,14 +40,14 @@ class SMSCodeView(View):
 
         # 创建redis连接对象
         redis_conn = get_redis_connection('verify_codes')
-        # 将redis中的图形验证码字符串获取出来.
+        # 将redis中的图形验证码字符串获取出来.这是一个字节类型的数据
         image_code_server_bytes = redis_conn.get(uuid)
         # 图形验证码从redis获取出来之后就从Redis数据库中删除:让图形验证码只能用一次
         redis_conn.delete(uuid)
         # 判断redis中是否获取到图形验证码(判断是否过期)
         if image_code_server_bytes is None:
             return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码错误'})
-        # 从redis获取出来的数据注意数据类型问题byte
+        # 从redis获取出来的数据注意数据类型问题byte，先进行decode操作
         image_code_server = image_code_server_bytes.decode()
         # 判断时要注意字典大小写问题
         if image_code_client.lower() != image_code_server.lower():
@@ -56,7 +56,7 @@ class SMSCodeView(View):
         # 发送短信验证码
         # 生成随机数作为短信验证码
         sms_code = '%06d'% random.randint(0, 999999)
-        # 将短信验证码存储到redis
+        # 将短信验证码存储到redis，key为了保持唯一，统一sms_手机号格式
         redis_conn.setex('sms_%s' % mobile, IMAGE_CODE_EXPIRE, sms_code)
         # 利用容联云平台发送短信
         # CCP().send_template_sms(手机号, [验证码, 过期时间：分钟], 使用的短信模板ID)
