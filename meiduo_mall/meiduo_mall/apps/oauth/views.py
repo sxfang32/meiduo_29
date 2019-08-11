467e0305ee142e60ef7517240869aad7e03e5e7f
sxfang32 <417036650@qq.com>
2019-08-11 15:59:12 +0800

    QQ登录：已绑定用户的情景

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,12 +1,16 @@
-import logging
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views import View
-from QQLoginTool.QQtool import OAuthQQ
 from django import http
+from django.contrib.auth import login
 
+from QQLoginTool.QQtool import OAuthQQ
 from meiduo_mall.utils.response_code import RETCODE
+from oauth.models import OAuthQQUser
+
+import logging
+
+logger = logging.getLogger('django')
 
 
 class QQAuthURLView(View):
@@ -32,6 +36,7 @@ class QQAuthURLView(View):
 
 class QQAuthUserView(View):
     """QQ登录成功后的回调处理"""
+
     def get(self, request):
 
         # 获取查询参数中的code
@@ -49,10 +54,22 @@ class QQAuthUserView(View):
             # 调用get_openid
             openid = auth_qq.get_open_id(access_token)
         except Exception as e:
-            logging.info(e)
+            logger.error(e)
             return http.JsonResponse({'code': RETCODE.SERVERERR, 'errmsg': 'OAuth2.0认证失败'})
 
-        # 去数据库中查询openID是否存在
-        # 如果不存在，说明openID还没有绑定美多中的用户，应该去绑定
-        # 如果存在，说明openID之前已经绑定过美多用户，那么直接代表登录成功
-        pass
+        try:
+            # 去数据库中查询openID是否存在
+            auth_model = OAuthQQUser.objects.get(openid=openid)
+        except OAuthQQUser.DoesNotExist:
+            pass
+            # 如果不存在，说明openID还没有绑定美多中的用户，应该去绑定
+        else:
+            # 如果存在，说明openID之前已经绑定过美多用户，那么直接代表登录成功
+            user = auth_model.user
+            # 状态保持
+            login(request, user)
+            # 向cookie存储username
+            response = redirect(request.GET.get('state') or '/')
+            response.set_cookie('username', user.username, max_age=settings.SSIONSE_COOKIE_AGE)
+            # 重定向到指定的来源页
+            return response
