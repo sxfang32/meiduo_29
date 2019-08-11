9643f867c498ff105aaf29a68a40bef3a4749c34
sxfang32 <417036650@qq.com>
2019-08-11 17:05:41 +0800

    QQ绑定用户实现

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -3,12 +3,13 @@ from django.conf import settings
 from django.views import View
 from django import http
 from django.contrib.auth import login
-
+import re
+from django_redis import get_redis_connection
 from QQLoginTool.QQtool import OAuthQQ
 from meiduo_mall.utils.response_code import RETCODE
 from oauth.models import OAuthQQUser
-from .utils import generate_openid_signature
-
+from .utils import generate_openid_signature, check_openid_signature
+from users.models import User
 import logging
 
 logger = logging.getLogger('django')
@@ -80,14 +81,57 @@ class QQAuthUserView(View):
     def post(self, request):
         """openID绑定用户逻辑"""
         # 接收表单数据
+        query_dict = request.POST
+        mobile = query_dict.get('mobile')
+        password = query_dict.get('password')
+        sms_code = query_dict.get('sms_code')
+        openid_sign = query_dict.get('openid')
+        # 校验数据
+        if all([mobile, password, sms_code, openid_sign]) is False:
+            return http.HttpResponseForbidden('参数不全，请重新输入')
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        if not re.match(r'^[a-zA-Z0-9]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
 
-        # 校验
+        # 短信验证码校验
+        # 创建redis连接对象
+        redis_conn = get_redis_connection('verify_codes')
+        # 将redis中的短信验证码字符串获取出来
+        sms_code_server_bytes = redis_conn.get('sms_%s' % mobile)
+        # 短信验证码从redis获取出来之后就从Redis数据库中删除:让图形验证码只能用一次
+        redis_conn.delete('sms_%s' % mobile)
+        # 判断redis中是否获取到短信验证码(判断是否过期)
+        if sms_code_server_bytes is None:
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '短信验证码错误'})
+        # 从redis获取出来的数据注意数据类型问题byte
+        sms_code_server = sms_code_server_bytes.decode()
+        # 判断短信验证码是否相等
+        if sms_code != sms_code_server():
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '短信验证码输入错误'})
+
+        # 对openID进行解密
+        openid = check_openid_signature(openid_sign)
+        if openid is None:
+            return http.HttpResponseForbidden('openid无效')
+        try:
+            # 以mobile字段进行查询user表
+            # 如果查到了，说明此手机号在美多商城之前已经注册，老用户
+            user = User.objects.get(mobile=mobile)
+        except User.DoesNotExist:
+            # 如果没有查询到，说明此手机号时新的，创建一个新的user
+            User.objects.create_user(username=mobile, password=password, mobile=mobile)
 
         # 新增oauth_qq表的一个记录
-
+        try:
+            OAuthQQUser.objects.create(user=user, openid=openid)
+        except:
+            return render(request, 'oauth_callback.html', {'qq_login_errmsg':'QQ登录失败'})
         # 绑定完成即代表登录成功
-
         # 状态保持
-        # 将username保存到cookie中
+        login(request, user)
 
-        pass
+        # 将username保存到cookie中
+        response = redirect(request.GET.get('state') or '/')
+        response.set_cookie('username', user.username, max_age=settings.SESSION_COOKIE_AGE)
+        return response
