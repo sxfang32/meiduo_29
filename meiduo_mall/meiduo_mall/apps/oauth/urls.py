0771ac2037d375d02c6ed1bf39a6b7de35e215b4
sxfang32 <417036650@qq.com>
2019-08-11 15:12:24 +0800

    完成oauth2.0获取openid

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+    url(r'^oauth_callback/$', views.QQAuthUserView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render
 from django.conf import settings
 from django.views import View
@@ -26,3 +28,24 @@ class QQAuthURLView(View):
 
         # 响应
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'login_url': login_url})
+
+
+class QQAuthUserView(View):
+    """QQ登录成功后的回调处理"""
+    def get(self, request):
+
+        # 获取查询参数中的code
+        code = request.GET.get('code')
+        # 校验
+        if code is None:
+            return http.HttpResponseForbidden('缺少code')
+        # 创建QQ登录工具对象
+        auth_qq = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                          client_secret=settings.QQ_CLIENT_SECRET,
+                          redirect_uri=settings.QQ_REDIRECT_URI)
+        # 调用get_access_token
+        access_token = auth_qq.get_access_token(code)
+        # 调用get_openid
+        openid = auth_qq.get_open_id(code)
+
+        pass
