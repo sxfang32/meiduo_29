76f58a3cabd23c4250df03b3bb475e3bb8f88ab2
sxfang32 <417036650@qq.com>
2019-08-17 12:03:13 +0800

    商品分页

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from contents.utls import get_categories
 from .models import GoodsCategory
 from django import http
+from django.core.paginator import Paginator
 
 class ListView(View):
     """商品列表界面"""
@@ -25,7 +26,11 @@ class ListView(View):
         }
 
 
-
+        # 查询指定三级类别下的所有SKU
+        sku_qs = cat3.sku_set.filter(is_launched=True)
+        # 每页显示5条数据:计算总页数 page = total_count / 5 + (1 if total % 5 else 0)
+        # sa = 5 * (page_num - 1)
+        # sku_qs[sa : (sa + (5 - 1))]  # 计算显示指定页的角标
 
         context = {
             'categories': get_categories(),  # 频道分类
@@ -34,6 +39,6 @@ class ListView(View):
             'category': '', # 第三级分类
             'page_skus': '',  # 分页后的数据
             'total_page': '', # 总页数
-            'page_num': page_num  # 翻页数
+            'page_num': page_num  # 当前页码
         }
         return render(request, 'list.html', context)
