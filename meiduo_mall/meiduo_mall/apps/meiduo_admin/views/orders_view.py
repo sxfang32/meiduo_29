1f9b3d77fc4e546f0f4692025f96ed7b5f24294b
sxfang32 <417036650@qq.com>
2019-09-05 10:47:02 +0800

    自定义权限认证

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/orders_view.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/orders_view.py
@@ -1,14 +1,26 @@
-from rest_framework.generics import GenericAPIView,UpdateAPIView
+from rest_framework.generics import GenericAPIView, UpdateAPIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from orders.models import OrderInfo
 from meiduo_admin.serializers.orders_serializer import *
 from meiduo_admin.pages import MyPage
+from rest_framework.permissions import IsAdminUser, BasePermission
+
+
+class MyAddOrderInfo(BasePermission):
+    """
+    自定权限，有没有对OrderInfo这样表对"增加"权限
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.has_perm("orders.add_orderinfo")
 
 
 class OrderInfoView(ListAPIView):
     queryset = OrderInfo.objects.all().order_by('-create_time')
     serializer_class = OrderInfoSerializer
     pagination_class = MyPage
+
+    permission_classes = [MyAddOrderInfo]
 
     def get_queryset(self):
         """实现根据keyword过滤order_id"""
@@ -21,4 +33,3 @@ class OrderInfoView(ListAPIView):
 class OrderInfoDetailView(RetrieveAPIView, UpdateAPIView):
     queryset = OrderInfo.objects.all()
     serializer_class = OrderInfoDetailSerializer
-
